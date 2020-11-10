0f9285da34a64cedeafe5ed452c980b4bec484c7
Jono Prest <jjprest@gmail.com>
2020-11-10 10:31:47 +0200

    cleaned up files and compressed images, changed colours

--- assets/js/main.js
+++ assets/js/main.js
@@ -154,6 +154,7 @@
   // Testimonials carousel (uses the Owl Carousel library)
   $(".testimonials-carousel").owlCarousel({
     autoplay: true,
+    autoplayTimeout: 7000,
     dots: true,
     loop: true,
     items: 1
